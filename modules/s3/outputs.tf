f1d36836bc1c23bc0feea70173b28dbdbee0105e
Drtom1 <tomogbonna11@gmail.com>
2026-05-23 16:08:16 +0100

    Add outputs for S3 logging bucket name and ARN

--- modules/s3/outputs.tf
+++ modules/s3/outputs.tf
@@ -12,3 +12,13 @@ output "bucket_domain_name" {
   description = "Domain name of the S3 bucket"
   value       = aws_s3_bucket.main.bucket_domain_name
 }
+
+output "logging_bucket_name" {
+  description = "Name of the S3 logging bucket (if enabled)"
+  value       = try(aws_s3_bucket.logging[0].bucket, null)
+}
+
+output "logging_bucket_arn" {
+  description = "ARN of the S3 logging bucket (if enabled)"
+  value       = try(aws_s3_bucket.logging[0].arn, null)
+}
